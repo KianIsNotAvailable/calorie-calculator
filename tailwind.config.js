b0433f95d38b9b9baad508de94600154de952659
kian <138216983+KianIsNotAvailable@users.noreply.github.com>
2023-10-27 15:57:52 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  
+  theme: {
+    fontFamily: {
+      sans: ['Poppins Regular', 'sans'],
+      
+    },
+    extend: {},
+  },
+  plugins: [
+  ],
+  variants: {
+    extend: {
+      backgroundColor: ['hover'],
+    },
+  },
+}
